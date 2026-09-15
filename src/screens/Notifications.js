cb84c440818642a9250d41267ef9bc37a0e4cfd7
janahasan <99503558+jana-alhasan@users.noreply.github.com>
2026-09-15 12:36:26 +0300

    Refactor import path for notifications styles
    Updated the import path for notifications styles.

--- src/screens/Notifications.js
+++ src/screens/Notifications.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text, FlatList} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {notificationsStyle} from './notificationsStyles';
+import {notificationsStyle} from './styles';
 
 // Mock data
 const notifications = [
